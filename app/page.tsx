2b99c6fe6c270470f8c7e224e306d54f99768e11
Explorarthecreator <fredadejiemma99@gmail.com>
2026-03-08 22:30:49 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
-import { geistMono, ppneuemontreal, rocoleta } from "./font";
+import Image from "next/image";
+import { rocoleta } from "./font";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="space-y-2">
-        <p className="text-2xl">God is the greatest</p>
-        <p className={`text-3xl ${ppneuemontreal.className}`}>Let God be God</p>
-        <p className={`text-3xl ${geistMono.className}`}>Let God be God</p>
-        <p className={`text-3xl ${rocoleta.className}`}>Let God be God</p>
+      <div className="space-y-2 h-60">
+        <Image
+          src={"/logo.svg"}
+          width={1000}
+          height={1000}
+          alt="Logo"
+          className="h-full animate-bounce "
+        />
+        <p
+          className={`${rocoleta.className} text-center text-xl lg:text-3xl animate-pulse`}
+        >
+          Work in progress
+        </p>
       </div>
     </div>
   );
