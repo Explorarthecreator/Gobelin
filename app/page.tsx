74590ef8ca9f8f03d3ced2317db19d65f878d94a
Explorarthecreator <fredadejiemma99@gmail.com>
2026-03-09 17:51:11 +0100

    fea: news

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/sections/about";
 import Metrics from "@/components/sections/metrics";
+import News from "@/components/sections/news";
 import Programmes from "@/components/sections/programmes";
 
 export default function Home() {
@@ -8,10 +9,7 @@ export default function Home() {
       <About />
       <Metrics />
       <Programmes />
-      <div className="mt-20"></div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-      eligendi porro consequuntur saepe laborum, sunt sint mollitia nulla amet
-      maxime.
+      <News />
     </main>
   );
 }
